15b4ff079de180d45e0e25b6b1014440a05f4dd9
jcarrera94 <juankarrera@gmail.com>
2019-09-16 20:38:24 +0000

    tail method added into lotide

---
+++ tail.js
@@ -0,0 +1,23 @@
+var emoji = require('node-emoji');
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${emoji.get(':v:')} ${emoji.get(':v:')} ${emoji.get(':v:')} Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${emoji.get(':-1:')} ${emoji.get(':-1:')} ${emoji.get(':-1:')} Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+let tail =[];
+const getTail = function(array) {
+
+  for (let i = 1; i < array.length; i++) {
+    tail.push(array[i]);
+  }
+  console.log(tail);
+  return tail;
+};
+
+const words = ['hola', 'Lighthouse', 'Labs'];
+getTail(words);
+assertEqual(tail[1], 'Labs');
