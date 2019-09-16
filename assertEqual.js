e26f74f5c0d3237214bea974a1be06bec9f3361e
jcarrera94 <juankarrera@gmail.com>
2019-09-16 19:58:35 +0000

    updating assertEqual.js with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,9 @@ var emoji = require('node-emoji');
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(emoji.get(':v:') + emoji.get(':v:') + emoji.get(':v:') + ' Assertion passed: ' + actual + ' === ' + expected);
+    console.log(`${emoji.get(':v:')} ${emoji.get(':v:')} ${emoji.get(':v:')} Assertion failed: ${actual} === ${expected}`);
   } else {
-    console.log(emoji.get(':-1:') + emoji.get(':-1:') + emoji.get(':-1:') + ' Assertion failed: ' + actual + ' !== ' + expected);
+    console.log(`${emoji.get(':-1:')} ${emoji.get(':-1:')} ${emoji.get(':-1:')} Assertion failed: ${actual} !== ${expected}`);
   }
 };
 
