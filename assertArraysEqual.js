999851c8c4b14f341518e9efe039789cd81c7970
jcarrera94 <juankarrera@gmail.com>
2019-09-17 21:54:57 +0000

    update previous files and added assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,29 @@
+let emoji = require('node-emoji');
+
+const eqArrays = function(arrayA, arrayB) {
+  let result = null;
+
+  if (arrayA.length !== arrayB.length) {
+    result = false;
+  }
+  for (let i = 0; i < arrayA.length; i++) {
+    if (arrayA[i] === arrayB[i]) {
+      result = true;
+    } else {
+      result = false;
+    }
+  }
+  return result;
+}
+
+const assertArraysEqual = function(array1, array2) {
+  let assertion = eqArrays(array1, array2);
+  if (assertion) {
+    console.log(`${emoji.get(':white_check_mark:')} ${emoji.get(':v:')} Assertion passed!`);
+  } else {
+    console.log(`${emoji.get(':red_circle:')} ${emoji.get(':-1:')} Assertion failed!`)
+  }
+
+}
+
+assertArraysEqual([1, 2, 3], [1, 2, '3']);
--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,4 @@
-var emoji = require('node-emoji');
+let emoji = require('node-emoji');
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
