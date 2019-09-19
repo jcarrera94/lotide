359010fc901403fe21a455e0184e8f55b2591240
jcarrera94 <juankarrera@gmail.com>
2019-09-19 19:26:48 +0000

    adding findKey method

---
+++ findKey.js
@@ -0,0 +1,29 @@
+let emoji = require('node-emoji');
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${emoji.get(':white_check_mark:')} ${emoji.get(':v:')} Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${emoji.get(':red_circle:')} ${emoji.get(':-1:')} Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKey = function(obj, callback){
+  for (let key in obj) {
+    if (callback(obj[key].stars)) {
+      return key;
+    }
+  }
+}
+
+const obj = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+
+assertEqual(findKey(obj, x => (x === 2)), 'noma');
