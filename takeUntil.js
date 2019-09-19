8321a49d3ed92eef0cd2d42936893441b2851589
jcarrera94 <juankarrera@gmail.com>
2019-09-19 19:00:03 +0000

    added take until method

---
+++ takeUntil.js
@@ -0,0 +1,50 @@
+let emoji = require('node-emoji');
+
+const eqArrays = function(arrayA, arrayB) {
+  let result = null;
+
+  if (arrayA.length !== arrayB.length) {
+    return false;
+  }
+  if (arrayA.length === 0 && arrayB.length === 0) {
+    result = true;
+  }
+  for (let i = 0; i < arrayA.length; i++) {
+    if (arrayA[i] === arrayB[i]) {
+      result = true;
+    } else {
+      return false;
+    }
+  }
+  return result;
+}
+
+const assertArraysEqual = function(array1, array2) {
+  let assertion = eqArrays(array1, array2);
+  if (assertion) {
+    console.log(`${emoji.get(':white_check_mark:')} ${emoji.get(':v:')} Assertion passed!`);
+  } else {
+    console.log(`${emoji.get(':red_circle:')} ${emoji.get(':-1:')} Assertion failed!`)
+  }
+}
+
+
+const takeUntil = function(array, callback) {
+  let result = [];
+  for (let x of array) {
+    if(callback(x)) {
+      return result;
+    }
+    result.push(x);
+  }
+  return result;
+}
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const test1 = [1, 2, 5, 7, 2];
+assertArraysEqual(takeUntil(data1, x => x < 0), test1);
+console.log('-----------------------');
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const test2 = ["I've", "been", "to", "Hollywod"];
+assertArraysEqual(takeUntil(data2, item => item === ","), test2);
