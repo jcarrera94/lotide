a6b7054398bb862724d32976e0f652e1b5d9b5fd
jcarrera94 <juankarrera@gmail.com>
2019-09-23 17:21:18 +0000

    refactored asserArraysEqual and created tests in a separate directory

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,5 @@
+const eqArrays = require('./eqArrays');
 let emoji = require('node-emoji');
-
-const eqArrays = function(arrayA, arrayB) {
-  let result = null;
-
-  if (arrayA.length !== arrayB.length) {
-    return false;
-  }
-  if (arrayA.length === 0 && arrayB.length === 0) {
-    result = true;
-  }
-  for (let i = 0; i < arrayA.length; i++) {
-    if (arrayA[i] === arrayB[i]) {
-      result = true;
-    } else {
-      return false;
-    }
-  }
-  return result;
-}
 
 const assertArraysEqual = function(array1, array2) {
   let assertion = eqArrays(array1, array2);
@@ -28,4 +10,5 @@ const assertArraysEqual = function(array1, array2) {
   }
 }
 
-assertArraysEqual([1, 2, 3], [1, 2, '3']);
+module.exports = assertArraysEqual;
+
